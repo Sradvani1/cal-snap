51bd6d98171ca49b940184dcc63b2e11ce8cd955
Sameer Advani <sradvani1@gmail.com>
2026-08-18 22:40:20 -0700

    fix(log): lift add meal drawer above nav

--- calsnap-web/components/meal-log/AddMealSheet.tsx
+++ calsnap-web/components/meal-log/AddMealSheet.tsx
@@ -29,7 +29,7 @@ export function AddMealSheet({
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
         <Drawer.Content
           className="fixed bottom-0 left-0 right-0 flex flex-col rounded-t-2xl bg-cs-surface"
-          style={{ bottom: 'var(--app-tab-bar-content-height, 0px)' }}
+          style={{ bottom: 'calc(var(--app-tab-bar-content-height, 0px) + 0.75rem)' }}
           aria-describedby={undefined}
         >
           <div className="mx-auto mt-2 h-1.5 w-12 rounded-full bg-cs-muted/30" />
